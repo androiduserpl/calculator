9fa498079cb14842cdaecc2bc14b13fc5ded41df
Adrian Nowakowski <androiduserpl@gmail.com>
2019-07-24 16:07:32 +0200

    1

---
+++ js.js
@@ -0,0 +1,22 @@
+var UIControler = function() {
+    var DOMStrings = {
+        button:  '.btn-num',
+        display: '.view',
+        equals: '.equals',
+    }
+
+    return {
+        getDOMStrings: function () {
+            return DOMStrings;
+        }
+    }
+}
+
+var valueOfButton =
+
+
+var EventListener = function (UIControler) {
+    var DOM = UIControler.getDOMStrings();
+
+    document.querySelector(DOM.button).addEventListener('clck', )
+}
